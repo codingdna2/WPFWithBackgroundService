fd08665a2b949c391d42525bc493c20b4ad2705a
Dan <codingdna@gmail.com>
2020-06-16 16:34:50 +0200

    Initial commit

---
+++ WPFWithBackgroundService/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+using WPFWithBackgroundService.Services;
+
+namespace WPFWithBackgroundService
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IHost _host;
+
+        public static IHostBuilder CreateHostBuilder() => new HostBuilder().ConfigureAppConfiguration((context, configurationBuilder) =>
+        {
+            configurationBuilder.AddEnvironmentVariables();
+            configurationBuilder.SetBasePath(context.HostingEnvironment.ContentRootPath);
+        });
+
+        public App()
+        {
+            _host = CreateHostBuilder().ConfigureServices((context, services) =>
+            {
+                services.AddHostedService<MyService>();
+                services.AddSingleton<MainWindow>();
+            }).Build();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
+
+        private async void OnApplicationExit(object sender, ExitEventArgs e)
+        {
+            await _host.StopAsync();
+
+            _host.Dispose();
+
+            _host = null;
+        }
+
+        private async void OnApplicationStartup(object sender, StartupEventArgs e)
+        {
+            await _host.StartAsync();
+
+            var mainWindow = _host.Services.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ WPFWithBackgroundService/Services/MyService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WPFWithBackgroundService.Services
+{
+    public class MyService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(10, stoppingToken);
+            }
+
+            //return Task.CompletedTask;
+        }
+    }
+}
